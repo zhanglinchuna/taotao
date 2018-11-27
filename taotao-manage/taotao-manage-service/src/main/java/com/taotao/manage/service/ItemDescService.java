de28f0cd93edf5f67a96ab11c296d5a0a011588a
zhanglinchun <2601165134@qq.com>
2018-11-27 22:30:33 +0800

    add

---
+++ taotao-manage/taotao-manage-service/src/main/java/com/taotao/manage/service/ItemDescService.java
@@ -0,0 +1,10 @@
+package com.taotao.manage.service;
+
+import com.taotao.pojo.ItemDesc;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ItemDescService extends BaseService<ItemDesc> {
+
+
+}
---
+++ taotao-manage/taotao-manage-service/src/main/java/com/taotao/manage/service/ItemService.java
@@ -0,0 +1,8 @@
+package com.taotao.manage.service;
+
+import com.taotao.pojo.Item;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ItemService extends BaseService<Item> {
+}
